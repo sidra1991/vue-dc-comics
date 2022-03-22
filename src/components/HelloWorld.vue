42bd4496dd0a61aef52492dbfbe0f381da0092ed
sidra1991 <1991sidra@gmail.com>
2022-03-22 20:37:59 +0100

    recupero domani

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <header>
+      <div class="header-left">
+        <img alt="Vue logo" src="../assets/img/dc-logo.png">
+      </div>
+      <div class="header-right" >
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#"> COMICS </a></li>
+          <li><a href="#"> MOVIE </a></li>
+          <li><a href="#"> TV </a></li>
+          <li><a href="#"> GAMES </a></li>
+          <li><a href="#"> COLLECTIBLES</a> </li>
+          <li><a href="#"> VIDEOS </a></li>
+          <li><a href="#"> FANS </a></li>
+          <li><a href="#"> NEWS </a></li>
+          <li><a href="#"> SHOP </a></li>
+        </ul>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -41,18 +33,48 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+header {
+  margin-top: 20px;
+  display: flex;
+  margin-top: 0;
+  height: 100px;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.header-left{
+  width: 50%;
+  height: 100%;
+  text-align: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.header-right {
+  overflow: hidden;
+  height: 100%;
+  ul{
+    height: 100%;
+    li{
+      height: 100%;
+      display: inline-block;
+      margin: 0.2rem;
+      position: relative;
+      a{
+        line-height: 100px;
+        display: inline-block;
+        height: 95%;
+        text-decoration: none;
+        color: black;
+        border-bottom: 3px solid transparent;
+        &:hover{
+          color:blue;
+          border-bottom:3px solid blue;
+        }
+      }
+    }
+  }
 }
-a {
-  color: #42b983;
+
+img {
+  width: 100px;
 }
+
 </style>
